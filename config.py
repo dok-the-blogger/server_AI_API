cdaae4e5e5ee17c1e1062b6e3bcc81ab98c497a6
dok-the-blogger <140554900+dok-the-blogger@users.noreply.github.com>
2026-03-03 14:39:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     API_TOKEN: str = ""        # Bearer-токен для авторизации
     GIGACHAT_CREDENTIALS: str = "" # Креды для GigaChat
+    GIGACHAT_ID: str = ""
     HOST: str = "127.0.0.1"
     PORT: int = 9000
     LOG_LEVEL: str = "info"
